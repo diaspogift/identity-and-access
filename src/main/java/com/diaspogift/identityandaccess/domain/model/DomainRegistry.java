e0187abb75b8ecaf036bfd107a9f8fa424e263e9
Nkalla Ehawe Didier Junior <didnkallaehawe@gmail.com>
2017-09-06 16:14:33 +0100

    Backupof 06-09-2017:1

---
+++ src/main/java/com/diaspogift/identityandaccess/domain/model/DomainRegistry.java
@@ -0,0 +1,78 @@
+//   Copyright 2012,2013 Vaughn Vernon
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package com.diaspogift.identityandaccess.domain.model;
+
+
+import com.diaspogift.identityandaccess.domain.model.access.AuthorizationService;
+import com.diaspogift.identityandaccess.domain.model.access.RoleRepository;
+import com.diaspogift.identityandaccess.domain.model.identity.*;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class DomainRegistry implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    public static AuthenticationService authenticationService() {
+        return (AuthenticationService) applicationContext.getBean("authenticationService");
+    }
+
+    public static AuthorizationService authorizationService() {
+        return (AuthorizationService) applicationContext.getBean("authorizationService");
+    }
+
+    public static EncryptionService encryptionService() {
+        return (EncryptionService) applicationContext.getBean("encryptionService");
+    }
+
+    public static GroupMemberService groupMemberService() {
+        return (GroupMemberService) applicationContext.getBean("groupMemberService");
+    }
+
+    public static GroupRepository groupRepository() {
+        return (GroupRepository) applicationContext.getBean("groupRepository");
+    }
+
+    public static PasswordService passwordService() {
+        return (PasswordService) applicationContext.getBean("passwordService");
+    }
+
+    public static RoleRepository roleRepository() {
+        return (RoleRepository) applicationContext.getBean("roleRepository");
+    }
+
+    public static TenantProvisioningService tenantProvisioningService() {
+        return (TenantProvisioningService) applicationContext.getBean("tenantProvisioningService");
+    }
+
+    public static TenantRepository tenantRepository() {
+        return (TenantRepository) applicationContext.getBean("tenantRepository");
+    }
+
+    public static UserRepository userRepository() {
+        return (UserRepository) applicationContext.getBean("userRepository");
+    }
+
+    @Override
+    public synchronized void setApplicationContext(
+            ApplicationContext anApplicationContext)
+    throws BeansException {
+
+        if (DomainRegistry.applicationContext == null) {
+            DomainRegistry.applicationContext = anApplicationContext;
+        }
+    }
+}
---
+++ src/main/java/com/diaspogift/identityandaccess/domain/model/identity/GroupUserAdded.java
@@ -0,0 +1,62 @@
+//   Copyright 2012,2013 Vaughn Vernon
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package com.diaspogift.identityandaccess.domain.model.identity;
+
+
+
+import com.diaspogift.identityandaccess.DomainEvent;
+
+import java.util.Date;
+
+public class GroupUserAdded implements DomainEvent {
+
+    private int eventVersion;
+    private String groupName;
+    private Date occurredOn;
+    private TenantId tenantId;
+    private String username;
+
+    public GroupUserAdded(TenantId aTenantId, String aGroupName, String aUsername) {
+        super();
+
+        this.eventVersion = 1;
+        this.groupName = aGroupName;
+        this.occurredOn = new Date();
+        this.tenantId = aTenantId;
+        this.username = aUsername;
+    }
+
+    @Override
+    public int eventVersion() {
+        return this.eventVersion;
+    }
+
+    public String groupName() {
+        return this.groupName;
+    }
+
+    @Override
+    public Date occurredOn() {
+        return this.occurredOn;
+    }
+
+    public TenantId tenantId() {
+        return this.tenantId;
+    }
+
+    public String username() {
+        return this.username;
+    }
+}
