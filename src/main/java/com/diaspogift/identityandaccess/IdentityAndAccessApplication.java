8e2e6ea95c4b713336d3050ab5c7666827b07fc4
Nkalla Ehawe Didier Junior <didnkallaehawe@gmail.com>
2017-09-06 10:45:24 +0100

    First commit in date of 06-09-2017

---
+++ src/main/java/com/diaspogift/identityandaccess/IdentityAndAccessApplication.java
@@ -0,0 +1,12 @@
+package com.diaspogift.identityandaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityAndAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityAndAccessApplication.class, args);
+	}
+}
