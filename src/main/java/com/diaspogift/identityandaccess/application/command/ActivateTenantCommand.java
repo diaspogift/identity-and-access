334049ac499005f861a8ea3913b8a132cc524a1e
diaspogift <diaspogift@gmail.com>
2019-11-11 13:41:49 -0800

    Update ActivateTenantCommand.java

--- src/main/java/com/diaspogift/identityandaccess/application/command/ActivateTenantCommand.java
+++ src/main/java/com/diaspogift/identityandaccess/application/command/ActivateTenantCommand.java
@@ -1,1 +1,24 @@
-package com.diaspogift.identityandaccess.application.command;public class ActivateTenantCommand {    private String tenantId;    public ActivateTenantCommand(String tenantId) {        super();        this.tenantId = tenantId;    }    public ActivateTenantCommand() {        super();    }    public String getTenantId() {        return this.tenantId;    }    public void setTenantId(String tenantId) {        this.tenantId = tenantId;    }}
+package com.diaspogift.identityandaccess.application.command;
+
+public class ActivateTenantCommand {
+    private String tenantId;
+
+    public ActivateTenantCommand(String tenantId) {
+        super();
+
+        this.tenantId = tenantId;
+    }
+
+    public ActivateTenantCommand() {
+        super();
+    }
+
+    public String getTenantId() {
+        return this.tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+}
+
