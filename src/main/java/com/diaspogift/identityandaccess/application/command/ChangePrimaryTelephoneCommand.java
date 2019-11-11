d037b9e3ceb19b683ad3613f8b4c1a354a897b15
diaspogift <diaspogift@gmail.com>
2019-11-11 13:47:18 -0800

    Update ChangePrimaryTelephoneCommand.java

--- src/main/java/com/diaspogift/identityandaccess/application/command/ChangePrimaryTelephoneCommand.java
+++ src/main/java/com/diaspogift/identityandaccess/application/command/ChangePrimaryTelephoneCommand.java
@@ -1,1 +1,65 @@
-package com.diaspogift.identityandaccess.application.command;public class ChangePrimaryTelephoneCommand {    private String tenantId;    private String username;    private String countryCode;    private String dialingCountryCode;    private String telephone;    public ChangePrimaryTelephoneCommand(String tenantId, String username, String countryCode, String dialingCountryCode, String telephone) {        super();        this.tenantId = tenantId;        this.username = username;        this.countryCode = countryCode;        this.dialingCountryCode = dialingCountryCode;        this.telephone = telephone;    }    public ChangePrimaryTelephoneCommand() {        super();    }    public String getTenantId() {        return this.tenantId;    }    public void setTenantId(String tenantId) {        this.tenantId = tenantId;    }    public String getUsername() {        return this.username;    }    public void setUsername(String username) {        this.username = username;    }    public String getTelephone() {        return this.telephone;    }    public void setTelephone(String telephone) {        this.telephone = telephone;    }    public String getCountryCode() {        return countryCode;    }    public void setCountryCode(String countryCode) {        this.countryCode = countryCode;    }    public String getDialingCountryCode() {        return dialingCountryCode;    }    public void setDialingCountryCode(String dialingCountryCode) {        this.dialingCountryCode = dialingCountryCode;    }}
+package com.diaspogift.identityandaccess.application.command;
+
+public class ChangePrimaryTelephoneCommand {
+    private String tenantId;
+    private String username;
+    private String countryCode;
+    private String dialingCountryCode;
+    private String telephone;
+
+
+    public ChangePrimaryTelephoneCommand(String tenantId, String username, String countryCode, String dialingCountryCode, String telephone) {
+        super();
+
+        this.tenantId = tenantId;
+        this.username = username;
+        this.countryCode = countryCode;
+        this.dialingCountryCode = dialingCountryCode;
+        this.telephone = telephone;
+    }
+
+    public ChangePrimaryTelephoneCommand() {
+        super();
+    }
+
+    public String getTenantId() {
+        return this.tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTelephone() {
+        return this.telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getDialingCountryCode() {
+        return dialingCountryCode;
+    }
+
+    public void setDialingCountryCode(String dialingCountryCode) {
+        this.dialingCountryCode = dialingCountryCode;
+    }
+}
+
