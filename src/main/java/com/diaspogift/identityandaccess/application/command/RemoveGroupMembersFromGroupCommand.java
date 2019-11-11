d2be4635cd11a90e74f966dd653449b48e86b857
diaspogift <diaspogift@gmail.com>
2019-11-11 13:35:12 -0800

    Update RemoveGroupMembersFromGroupCommand.java

--- src/main/java/com/diaspogift/identityandaccess/application/command/RemoveGroupMembersFromGroupCommand.java
+++ src/main/java/com/diaspogift/identityandaccess/application/command/RemoveGroupMembersFromGroupCommand.java
@@ -1,1 +1,46 @@
-package com.diaspogift.identityandaccess.application.command;import com.diaspogift.identityandaccess.application.representation.group.GroupMemberRepresentation;import java.util.ArrayList;import java.util.Collection;public class RemoveGroupMembersFromGroupCommand {    private String tenantId;    private Collection<GroupMemberRepresentation> groupMembers = new ArrayList<GroupMemberRepresentation>();    private String parentGroupName;    public RemoveGroupMembersFromGroupCommand(String tenantId, String groupName, Collection<GroupMemberRepresentation> groupMembers) {        this.tenantId = tenantId;        this.groupMembers = groupMembers;        this.parentGroupName = groupName;    }    public String getTenantId() {        return tenantId;    }    public void setTenantId(String tenantId) {        this.tenantId = tenantId;    }    public Collection<GroupMemberRepresentation> getGroupMembers() {        return groupMembers;    }    public void setGroupMembers(Collection<GroupMemberRepresentation> groupMembers) {        this.groupMembers = groupMembers;    }    public String getParentGroupName() {        return parentGroupName;    }    public void setParentGroupName(String parentGroupName) {        this.parentGroupName = parentGroupName;    }}
+package com.diaspogift.identityandaccess.application.command;
+
+import com.diaspogift.identityandaccess.application.representation.group.GroupMemberRepresentation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class RemoveGroupMembersFromGroupCommand {
+
+    private String tenantId;
+    private Collection<GroupMemberRepresentation> groupMembers = new ArrayList<GroupMemberRepresentation>();
+    private String parentGroupName;
+
+
+    public RemoveGroupMembersFromGroupCommand(String tenantId, String groupName, Collection<GroupMemberRepresentation> groupMembers) {
+
+        this.tenantId = tenantId;
+        this.groupMembers = groupMembers;
+        this.parentGroupName = groupName;
+    }
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public Collection<GroupMemberRepresentation> getGroupMembers() {
+        return groupMembers;
+    }
+
+    public void setGroupMembers(Collection<GroupMemberRepresentation> groupMembers) {
+        this.groupMembers = groupMembers;
+    }
+
+    public String getParentGroupName() {
+        return parentGroupName;
+    }
+
+    public void setParentGroupName(String parentGroupName) {
+        this.parentGroupName = parentGroupName;
+    }
+}
+
